b32d599a7ad4cd75f556b245d2ef9c58541372e3
morlimoore <cooliyk2@gmail.com>
2020-11-15 22:06:22 +0100

    Creates UserServiceImpl

---
+++ src/main/java/com/morlimoore/apidemo/services/impl/UserServiceImpl.java
@@ -0,0 +1,63 @@
+package com.morlimoore.apidemo.services.impl;
+
+import com.morlimoore.apidemo.dtos.UserUpdateDto;
+import com.morlimoore.apidemo.entities.User;
+import com.morlimoore.apidemo.repositories.UserRepository;
+import com.morlimoore.apidemo.services.UserService;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
+
+    public UserServiceImpl(UserRepository userRepository, ModelMapper modelMapper) {
+        this.userRepository = userRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public List<User> listAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+
+    @Override
+    public User getUser(Long id) {
+        return userRepository.findById(id).get();
+    }
+
+    @Override
+    public void updateUser(UserUpdateDto userUpdateDto, Long id) {
+        User user = modelMapper.map(userUpdateDto, User.class);
+        user.setId(id);
+        saveUser(user);
+    }
+
+    @Override
+    public void patchUser(UserUpdateDto userUpdateDto, Long id) {
+        User user = getUser(id);
+        if (!userUpdateDto.getFirstName().isBlank())
+            user.setFirstName(userUpdateDto.getFirstName());
+        if (!userUpdateDto.getLastName().isBlank())
+            user.setLastName(userUpdateDto.getLastName());
+        if (!userUpdateDto.getEmail().isBlank())
+            user.setEmail(userUpdateDto.getEmail());
+        userRepository.save(user);
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
