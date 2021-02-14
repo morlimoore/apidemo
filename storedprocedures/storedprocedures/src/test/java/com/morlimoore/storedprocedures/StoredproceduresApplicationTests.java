8aa16d4804721bc6671a390d40d757ca5df11eb5
morlimoore <cooliyk2@gmail.com>
2021-02-14 23:08:40 +0100

    Initial commit

---
+++ storedprocedures/storedprocedures/src/test/java/com/morlimoore/storedprocedures/StoredproceduresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morlimoore.storedprocedures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoredproceduresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
