70708b6e103459edfff91e0dcc63cdddab5e85f6
morlimoore <cooliyk2@gmail.com>
2021-02-17 02:08:06 +0100

    Removes jpa from model

--- storedprocedures/storedprocedures/src/main/java/com/morlimoore/storedprocedures/models/User.java
+++ storedprocedures/storedprocedures/src/main/java/com/morlimoore/storedprocedures/models/User.java
@@ -3,23 +3,13 @@ package com.morlimoore.storedprocedures.models;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
-
-@Entity
 @Getter
 @Setter
-@Table(name = "users")
-public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+public class User extends AbstractModel {
+
     private long id;
-
     private String firstName;
-
     private String lastName;
-
-    @Column(nullable = false, unique = true)
     private String email;
-
     private int age;
 }
